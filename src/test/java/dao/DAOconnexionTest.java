8f9d20c9ee789a9933465f60e43715155c58756a
Louis86 <louishuguesa@gmail.com>
2019-11-28 11:56:16 +0100

    first test

--- src/test/java/dao/DAOconnexionTest.java
+++ src/test/java/dao/DAOconnexionTest.java
@@ -1,10 +1,18 @@
 package dao;
 import static org.junit.jupiter.api.Assertions.*;
 
+import dao.DAOrequester;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 class DAOconnexionTest {
 
-    void innitConn() {
-
+    void innitConn() throws SQLException {
+        Connection conne=DriverManager.getConnection("db_tdd", "root", "");
+        stmte = conne.createStatement();
+        assertTrue(stmte);
     }
     void closeConn() {
     }
