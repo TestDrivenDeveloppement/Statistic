cb21608f67d52a34a5200991c162502558b1e7e1
Louis86 <louishuguesa@gmail.com>
2019-12-02 16:28:34 +0100

    add test Mathematical first

---
+++ src/test/java/mathematical/MathematicalStatsCalculatorTest.java
@@ -0,0 +1,47 @@
+package mathematical;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathematicalStatsCalculatorTest {
+
+  @Test
+  void getSommeHeureEmployeEntreprise() {
+
+
+
+  }
+
+  @Test
+  void getSommeHeureEmployeProjet() {
+  }
+
+  @Test
+  void getMoyenneHeureEmployeEntreprise() {
+  }
+
+  @Test
+  void getMoyenneHeureEmployeProjet() {
+  }
+
+  @Test
+  void getVarianceHeureEmployeEntreprise() {
+  }
+
+  @Test
+  void getVarianceHeureEmployeProjet() {
+  }
+
+  @Test
+  void getEcartTypeHeureEmployeEntreprise() {
+  }
+
+  @Test
+  void getEcartTypeHeureEmployeProjet() {
+  }
+
+  @Test
+  void superStatInd() {
+  }
+}
